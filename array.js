466b18bcf3fb0a2bfde85a1f7702aa91e4c8df92
redjerrk <ocean21ratul@gmail.com>
2024-04-07 21:31:33 +0600

    working with js

---
+++ array.js
@@ -0,0 +1,5 @@
+var ageValue = [121, 25, 56, 50, 54, 56, 30]
+
+
+console.log(ageValue.indexOf(56))
+console.log(ageValue[4])
--- first.js
+++ first.js
@@ -1,1 +1,20 @@
-console.log(5)
+
+var numberOne = 25;
+var numberTwo = "25";
+
+console.log(typeof numberOne);
+console.log(typeof numberTwo);
+
+var one = 2.5;
+var two = 5.5;
+
+var sum = one + two;
+sum = sum.toFixed(3);
+
+console.log(sum)
+
+
+var intNumber = parseInt(two)
+
+console.log(intNumber)
+
